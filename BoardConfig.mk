ff0870f99ff461a6d2c139d0a40c9d6260830014
rkpeterson <robertkpeterson9@gmail.com>
2015-05-19 11:40:39 -0500

    again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ GRAPHITE := true
 LTO := true
 A15_QUAD := true
 
-USE_PREBUILT_CACHE=1
+# USE_PREBUILT_CACHE=1
 
 TARGET_OTA_ASSERT_DEVICE := htc_m8,htc_m8whl,htc_m8wl,m8,m8wl,m8wlv,m8vzw,m8whl,m8spr
 TARGET_BOARD_INFO_FILE ?= device/htc/m8/board-info.txt
