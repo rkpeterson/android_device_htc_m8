15de4d3a4b93672c96d5fcc678145f2523dc818a
rkpeterson <robertkpeterson9@gmail.com>
2015-05-19 06:35:04 -0500

    use new opts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,13 @@
 # 0P6B20000 - Verizon
 # 0P6B70000 - Sprint
 
-LIQUIFY=true
+USE_O3 := true
+STRICT := true
+GRAPHITE := true
+LTO := true
+A15_QUAD := true
+
+USE_PREBUILT_CACHE=1
 
 TARGET_OTA_ASSERT_DEVICE := htc_m8,htc_m8whl,htc_m8wl,m8,m8wl,m8wlv,m8vzw,m8whl,m8spr
 TARGET_BOARD_INFO_FILE ?= device/htc/m8/board-info.txt
