LineageOS/android_device_htc_m8d

init: Update defines and fix build
Change-Id: I7dda0f11d8ea8aaf47d6d9b1c924f3a53919c919

BoardConfig.mk
@@ -54,8 +54,8 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 11140071424
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_m8d
-TARGET_LIBINIT_DEFINES_FILE := device/htc/m8d/init/init_m8d.c
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/htc/m8d/init/init_m8d.cpp
 
 # Inherit from the proprietary version
 -include vendor/htc/m8d/BoardConfigVendor.mk

rkpeterson/android_device_htc_m8

Change-Id: Id5607806889ae1b0add506fae314cab67b2cec2e

BoardConfig.mk
@@ -64,8 +64,8 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_m8
-TARGET_LIBINIT_DEFINES_FILE := device/htc/m8/init/init_m8.c
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/htc/m8/init/init_m8.cpp
 
 # Inherit from the proprietary version
 -include vendor/htc/m8/BoardConfigVendor.mk